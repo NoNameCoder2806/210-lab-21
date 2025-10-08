2b1c52bc7b4a263448f5e6140a0114506f70f695
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-10-07 17:13:57 -0700

    Copied DoublyLinkedList class

---
+++ Goat.h
@@ -0,0 +1,15 @@
+// COMSC-210 | Lab 21 | Dat Hoang Vien
+// IDE used: Visual Studio Code
+
+// Libraries
+#include <iostream>
+using namespace std;
+
+// Constants
+const int MIN_NR = 10;
+const int MAX_NR = 99;
+const int MIN_LS = 5;
+const int MAX_LS = 20;
+
+// DoublyLinkedList class
+class DoublyLinkedList
