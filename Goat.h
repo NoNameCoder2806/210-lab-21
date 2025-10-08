e4b4408707414f5c2b0e19226a2181b394389a36
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-10-07 17:23:45 -0700

    Setting up Goat class

--- Goat.h
+++ Goat.h
@@ -3,13 +3,29 @@
 
 // Libraries
 #include <iostream>
+#include <string>
 using namespace std;
 
 // Constants
-const int MIN_NR = 10;
-const int MAX_NR = 99;
-const int MIN_LS = 5;
-const int MAX_LS = 20;
+const int MAX_AGE = 20;        // The maximum age of a Goat
+const int NAMES = 15;          // The number of names
+const int COLORS = 15;         // The number of colors
 
-// DoublyLinkedList class
-class DoublyLinkedList
+// Goat class
+class Goat
+{
+private:
+    int age;             // The age of the Goat object
+    string name;         // The name of the Goat object
+    string color;        // The color of the Goat object
+
+    // Constant arrays
+    // Names
+    const string nameList[NAMES] = {"Tiny", "Old", "Mature", "Senior", "Junior", "Baby", "Teen", "Elder",
+                                    "Boss", "Legend", "Godlike", "Goober", "Speedy", "Chunky", "Smarty"};
+    // Colors
+    const string colorList[COLORS] = {"White", "Black", "Brown", "Gray", "Gold", "Red", "Yellow", "Orange",
+                                    "Cream", "Mauve", "Silver", "Tan", "Beige", "Copper", "Ivory"};
+
+public:
+};
