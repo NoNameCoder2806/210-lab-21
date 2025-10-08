427e9bb4574173a52c29ba91ccb377081b386547
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-10-07 17:02:58 -0700

    Setting up Lab21

---
+++ main.cpp
@@ -0,0 +1,24 @@
+// COMSC-210 | Lab 21 | Dat Hoang Vien
+// IDE used: Visual Studio Code
+
+// Libraries
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+
+// Constants
+const int SIZE = 3;
+
+// Function prototypes
+
+// Main function
+int main()
+{
+    cout << "Hello World" << endl;
+
+    return 0;
+}
+
+// Function implementations
